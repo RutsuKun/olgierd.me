4230a25c593759355138b1366f82ae5da8d12711
Olgierd Grzyb <kontakt@olgierd.me>
2016-01-17 20:13:41 +0100

    Initial commit with new design

---
+++ js/common.js
@@ -0,0 +1,27 @@
+var WebFontConfig = {
+  google: { families: [ 'Source+Serif+Pro:400,700:latin,latin-ext' ] }
+};
+(function() {
+  var wf = document.createElement('script');
+  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+
+  [].forEach.call(document.getElementsByTagName('h2'), function(titleHeader) {
+    var id = titleHeader.parentNode.id,
+        anchor;
+
+    if (id) {
+      anchor = document.createElement('a');
+      anchor.className = 'Box-anchor';
+      anchor.textContent = anchor.href = '#' + id;
+      titleHeader.appendChild(anchor);
+    }
+  });
+
+  var email = document.getElementById('email');
+  email.firstChild.textContent = ['.me', 'olgierd'].concat(['@'].concat('kontakt')).reverse().join('');
+  email.firstChild.href = [email.firstChild.textContent, 'mailto'].reverse().join(':');
+})();
---
+++ private/hello.php
@@ -0,0 +1,1 @@
+<?php header('HTTP/1.1 501 Not Implemented');
