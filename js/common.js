e09d594a97f686f780fa91684fd464bfc4c0cb55
Olgierd Grzyb <kontakt@olgierd.me>
2016-01-30 23:36:46 +0100

    Refactored js/common.js

--- js/common.js
+++ js/common.js
@@ -9,27 +9,37 @@ var WebFontConfig = {
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(wf, s);
 
-  var fortunes = [
-    'Deleted code is debugged code.',
-    'The cheapest, fastest, and most reliable components are those that aren’t there.'
-  ];
-  var fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  document.getElementsByClassName('Header-fortuneText')[0].textContent = fortune;
+  function setRandomFortune() {
+    var fortunes = [
+      'Deleted code is debugged code.',
+      'One of my most productive days was throwing away 1000 lines of code.',
+    ];
+    var fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    document.getElementsByClassName('Header-fortuneText')[0].textContent = fortune;
+  }
 
-  [].forEach.call(document.getElementsByTagName('h2'), function(titleHeader) {
-    var id = titleHeader.parentNode.id,
-        anchor;
+  function addSectionAnchors() {
+    [].forEach.call(document.getElementsByTagName('h2'), function(titleHeader) {
+      var id = titleHeader.parentNode.id,
+          anchor;
 
-    if (id) {
-      anchor = document.createElement('a');
-      anchor.className = 'Box-anchor';
-      anchor.href = '#' + id;
-      anchor.textContent = '¶';
-      titleHeader.appendChild(anchor);
-    }
-  });
+      if (id) {
+        anchor = document.createElement('a');
+        anchor.className = 'Box-anchor';
+        anchor.href = '#' + id;
+        anchor.textContent = '¶';
+        titleHeader.appendChild(anchor);
+      }
+    });
+  }
 
-  var email = document.getElementById('email');
-  email.firstChild.textContent = ['.me', 'olgierd'].concat(['@'].concat('kontakt')).reverse().join('');
-  email.firstChild.href = [email.firstChild.textContent, 'mailto'].reverse().join(':');
+  function showEmail() {
+    var email = document.getElementById('email');
+    email.firstChild.textContent = ['.me', 'olgierd'].concat(['@'].concat('kontakt')).reverse().join('');
+    email.firstChild.href = [email.firstChild.textContent, 'mailto'].reverse().join(':');
+  }
+  
+  showEmail();
+  setRandomFortune();
+  addSectionAnchors();
 })();
