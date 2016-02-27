8aba98349d46b58bfc51e5f95b6effbbf01af73f
Olgierd Grzyb <kontakt@olgierd.me>
2016-02-27 16:59:48 +0100

    Update configuration

--- config.js
+++ config.js
@@ -3,6 +3,11 @@ module.exports = {
   "description": "Jestem studentem. W kręgu moich zainteresowań leży przede wszystkim web development, czyli potocznie – robienie internetów, a także systemy Linux.",
   "allWritings": [
     {
+      "title": "Pobieranie rekurencyjne przy pomocy wget",
+      "description": "Jak łatwo pobrać całe drzewo folderów z serwera HTTP",
+      "url": "/wget/rekurencja"
+    },
+    {
       "title": "Zmiana hasła użytkownika Windows - Linuksem",
       "description": "Odzyskaj dostęp do systemu w kilku prostych krokach",
       "url": "/haslo"
